365a8e8e21064e44691fceef1b588684cabdd433
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-16 15:09:01 +0200

    [UPD] Added array with objects

--- snack-1/js/main.js
+++ snack-1/js/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const biciDaCorsa = [
+    {
+        nome: 'Scott',
+        peso: '10',
+    },
+    {
+        nome: 'Cannondale',
+        peso: '20',
+    },
+    {
+        nome: 'Genesis',
+        peso: '15',
+    },
+    {
+        nome: 'Focus',
+        peso: '5',
+    },
+    {
+        nome: '3T',
+        peso: '8',
+    }
+]
